5c789ebc2fe53d07f90c56bace785b8ee946afb9
eamon0989 <eamon0989@gmail.com>
2025-12-31 13:22:54 +0100

    chore: remove unused update company helper function

--- apps/backend/src/app/companies/util/updateCompabny.helpers.ts
+++
@@ -1,9 +0,0 @@
-import { UpdateCompanyDto } from "../dto/update-company.dto";
-import { CompanyEntity } from "../entities/company.entity";
-
-export function mapUpdate(company: CompanyEntity, updateDto: UpdateCompanyDto) {
-    updateDto.VAT && (company.VAT = updateDto.VAT)
-    updateDto.name &&(company.name = updateDto.name)
-
-    return company;
-}
